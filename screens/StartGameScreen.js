28278cd1b540528edf69c62d95d916534c3e8b8e
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-26 17:22:40 +0200

    created a basic input field with a input limit of 2 numbers

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,46 @@
-import {TextInput, View, Text} from 'react-native';
+import { TextInput, View, Text, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen (){
-return (
-<View>
-    <TextInput />
-    <Text></Text>
-</View>
-)
+function StartGameScreen() {
+  return (
+    <View style={styles.InputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  InputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    textAlign: "center",
+    height: 50,
+    width: 60,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+});
