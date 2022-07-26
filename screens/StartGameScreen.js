23f2b1357fea3bd73c43f70c26cd395c771ffef5
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-26 15:50:15 +0200

    created the folders with files for the different screens

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import {TextInput, View, Text} from 'react-native';
+
+function StartGameScreen (){
+return (
+<View>
+    <TextInput />
+    <Text></Text>
+</View>
+)
+}
+
+export default StartGameScreen;
