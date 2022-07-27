3bd4db1f8fa3e82cc511441ad817d46398133285
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-27 15:08:31 +0200

    component Title created

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen() {
   return(
   <View style={styles.screen}>
-    <Text>Opponent's Guess</Text>
+    <Title>Opponent's Guess</Title>
 
     <View>
         <Text>Higher or lower?</Text>
@@ -21,5 +22,14 @@ const styles = StyleSheet.create({
     screen: {
 flex: 1,
 padding: 24,
+    },
+    title: {
+       fontSize: 24,
+       fontWeight: "bold",
+       color: '#ddb52f',
+       textAlign: 'center',
+       borderWidth: 2,
+       borderColor: '#ddb52f',
+       padding: 12,
     },
 });
