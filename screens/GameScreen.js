11160f8fe77f5ee2fa76427bee2429f36747493a
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-28 08:07:58 +0200

    created the function generateRandom Between so it can calculate a number to guess

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,35 +2,43 @@ import { Text, View, StyleSheet } from "react-native";
 import Title from "../components/Title";
 import Colors from "./constants/colors";
 
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
 function GameScreen() {
-  return(
-  <View style={styles.screen}>
-    <Title>Opponent's Guess</Title>
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
 
-    <View>
+      <View>
         <Text>Higher or lower?</Text>
+      </View>
+      <View></View>
     </View>
-    <View>
-
-    </View>
-  </View>
-  )
+  );
 }
 
 export default GameScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-flex: 1,
-padding: 24,
-    },
-    title: {
-       fontSize: 24,
-       fontWeight: "bold",
-       color: Colors.accent500,
-       textAlign: 'center',
-       borderWidth: 2,
-       borderColor: Colors.accent500,
-       padding: 12,
-    },
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: Colors.accent500,
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: Colors.accent500,
+    padding: 12,
+  },
 });
